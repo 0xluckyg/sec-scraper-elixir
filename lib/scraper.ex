6c1ceccead353f45938badd566bf9eed6ac41494
Kevin Xu <1kevinxu1@gmail.com>
2017-01-21 15:29:29 -0500

    regex hipe

--- lib/scraper.ex
+++ lib/scraper.ex
@@ -17,10 +17,21 @@ defmodule Scraper do
 
   defp map_entry(entry) do
     IO.inspect(entry)
-    %{
-      title: get_title(entry),
-      link:  get_link(entry)
+    IO.puts("\n")
+    struct = %{
+      title:   get_title(entry),
+      link:    get_link(entry),
+      form:    get_form(entry),
+      file_id: get_file_id(entry)
     }
+    print_struct(struct)
+  end
+
+  defp print_struct(struct) do
+    IO.inspect(struct)
+    IO.puts("\n")
+    IO.puts("------------------------------------------------------------------")
+    struct
   end
 
   defp get_title(entry) do
@@ -33,4 +44,18 @@ defmodule Scraper do
     link
   end
 
+  defp get_form(entry) do
+    [%{attr: [_, _, {:term, term}]}] = Quinn.find(entry, :category)
+    term
+  end
+
+  def get_file_id(entry) do
+    [%{value: [accession_string]}] = Quinn.find(entry, :id)
+    get_accession_number(accession_string)
+  end
+
+  def get_accession_number(accession_string) do
+    regex = ~r/[a-z].+accession-number=(?<accession>[0-9-]+)/
+    Regex.named_captures(regex, accession_string)["accession"]
+  end
 end
