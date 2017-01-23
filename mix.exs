8263361290f2f5c7ea1b308b7d0e6fe748cf12d1
Kevin Xu <1kevinxu1@gmail.com>
2017-01-23 06:12:23 -0500

    Start umbrella project

--- mix.exs
+++ mix.exs
@@ -1,21 +1,11 @@
-defmodule Scraper.Mixfile do
+defmodule SecFilings.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :scraper,
-     version: "0.1.0",
-     elixir: "~> 1.3",
+    [apps_path: "apps",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
-  end
-
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
-  def application do
-    [mod: {Scraper, []},
-     applications: [:logger, :httpoison, :quinn, :postgrex, :ecto]]
+     deps: deps]
   end
 
   # Dependencies can be Hex packages:
@@ -26,13 +16,11 @@ defmodule Scraper.Mixfile do
   #
   #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
   #
-  # Type "mix help deps" for more examples and options
+  # Type "mix help deps" for more examples and options.
+  #
+  # Dependencies listed here are available only for this project
+  # and cannot be accessed from applications inside the apps folder
   defp deps do
-    [
-      { :httpoison, "~> 0.11.0" },
-      { :quinn, "~> 1.0.0" },
-      { :postgrex, ">=0.0.0" },
-      { :ecto, "~> 2.1.3" }
-    ]
+    []
   end
 end
