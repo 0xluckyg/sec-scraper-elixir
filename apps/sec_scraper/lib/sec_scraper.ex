63f5f2ffe892cdd97b585540978165043f4b8dac
Kevin Xu <1kevinxu1@gmail.com>
2017-02-01 15:48:56 -0500

    rip filing links from each file aggregation page

--- apps/sec_scraper/lib/sec_scraper.ex
+++ apps/sec_scraper/lib/sec_scraper.ex
@@ -12,18 +12,39 @@ defmodule SecScraper do
     Supervisor.start_link(children, opts)
   end
 
-  def go do
-    url = "https://www.sec.gov/Archives/edgar/data/1659166/000165916617000034/0001659166-17-000034-index.htm"
+  def go(url \\ "https://www.sec.gov/Archives/edgar/data/1682600/000006501117000049/0000065011-17-000049-index.htm") do
     HTTPoison.get!(url).body
+    |> Floki.find("table.tableFile")
     |> Floki.find("a")
-    |> Enum.each(fn(html_node) ->
-      {tag_name, attributes, children_nodes} = html_node
-      IO.inspect "tag name: "
-      IO.inspect tag_name
-      IO.inspect "attributes: "
-      IO.inspect attributes
-      IO.inspect "children nodes: "
-      IO.inspect children_nodes
+    |> get_links()
+    |> Enum.map(fn({key, node}) ->
+      {_tag, [{_href, link}], [_children]} = node
+      {key, link}
     end)
+    # |> save_all()
   end
+
+  def save_all(list) do
+    save(html: list[:html])
+    save(xml: list[:xml])
+    save(txt: list[:txt])
+  end
+
+  def get_links([html, xml, next | tail]) do
+    [html: html, xml: xml, txt: get_txt(tail, next)]
+  end
+
+  def get_txt([], last),  do: last
+  def get_txt([next | tail], _cur) do
+    get_txt(tail, next)
+  end
+
+  def save(html: html), do: html
+  def save(xml: xml), do: xml
+  def save(txt: txt), do: txt
+
+  # def process_node(html_node) do
+  #   {tag_name, attributes, children_nodes} = html_node
+  # end
+
 end
